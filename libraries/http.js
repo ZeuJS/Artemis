e9736b4f553e9fc1a859212e9e1797cdd39420f0
Gaël Demette <demette.gael@gmail.com>
2014-09-23 00:17:28 +0200

    * better syntax

--- libraries/http.js
+++ libraries/http.js
@@ -2,8 +2,8 @@
 var http = require('http');
 
 http.ServerResponse.prototype.json = function(json, httpCode) {
-    httpCode = typeof httpCode !== 'undefined' ? httpCode : 200;
-    json = typeof json !== 'undefined' ? json : { success: false, message: 'zeusjs.internal.artemis.noResponse' };
+    httpCode = httpCode || 200;
+    json = json || { success: false, message: 'zeusjs.internal.artemis.noResponse' };
     var header = {
         'Content-Type': 'application/json; charset=utf-8',
         'Server': 'ZeuJS/Artemis',
